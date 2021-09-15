de5594097c253ac4ed1570827fd6ce5b128349d3
Anton Reindl <toni.reindl@web.de>
2021-09-15 14:08:10 +0200

    docs: add slides

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto">
     <h3 class="text-center font-bold gray-700 uppercase mt-48 text-4xl">
-      Welcome to Anton's Bike Tours
+      Welcome to Anton's Bike Tours 🚵🏼‍♂️
     </h3>
     <nav class="mx-auto mt-10 text-center space-x-5 text-xl">
       <nuxt-link to="/blog" class="hover:text-green-600">Blog</nuxt-link>
