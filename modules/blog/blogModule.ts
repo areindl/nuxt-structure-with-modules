e4730aaa5bb4870f122dbcef8722481cd400f570
Anton Reindl <anton@mindfuel.ai>
2023-04-03 19:53:50 +0200

    fix: make ready for nuxt 3.3

---
+++ modules/blog/blogModule.ts
@@ -0,0 +1,39 @@
+import { defineNuxtModule } from '@nuxt/kit'
+import { resolve, join } from 'pathe'
+import type { Nuxt } from '@nuxt/schema'
+
+export default defineNuxtModule({
+  meta: {
+    // Usually the npm package name of your module - in this case a local modal
+    name: 'blog-module',
+    // The key in `nuxt.config` that holds the
+    configKey: 'blog-module',
+    // Compatibility constraints
+    compatibility: {
+      // Semver version of supported nuxt versions
+      nuxt: '^3.3.0',
+    },
+  },
+  setup(options: any, nuxt: Nuxt) {
+    // Auto register components
+    nuxt.hook('components:dirs', (dirs) => {
+      dirs.push({
+        path: join(__dirname, 'components'),
+      })
+    })
+
+    // Auto register composables
+    nuxt.hook('imports:extend', (dirs) => {
+      dirs.push(resolve(__dirname, './composables'))
+    })
+
+    // Auto register pages
+    nuxt.hook('pages:extend', (pages) => {
+      pages.push({
+        name: 'blog-page',
+        path: '/blog/:id',
+        file: resolve(__dirname, './pages/blog.vue'),
+      })
+    })
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt/config'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    // Load Blog module locally
-    './modules/blog',
-  ],
+  modules: ['~/modules/blog/blogModule'],
 })
